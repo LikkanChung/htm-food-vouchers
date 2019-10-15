8689c7b9543d4539f86733b1b7e8201cf1f56fa1
llc721 <llc721@student.bham.ac.uk>
2019-10-16 00:35:34 +0100

    Working Initial Prototype

---
+++ HTMFoodVoucher/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HTMFoodVoucher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.nudID = new System.Windows.Forms.NumericUpDown();
+            this.btn_PrintAndIncrement = new System.Windows.Forms.Button();
+            this.cmbVendor = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(12, 65);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(120, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print Current";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // nudID
+            // 
+            this.nudID.Location = new System.Drawing.Point(12, 39);
+            this.nudID.Name = "nudID";
+            this.nudID.Size = new System.Drawing.Size(120, 20);
+            this.nudID.TabIndex = 1;
+            // 
+            // btn_PrintAndIncrement
+            // 
+            this.btn_PrintAndIncrement.Location = new System.Drawing.Point(12, 94);
+            this.btn_PrintAndIncrement.Name = "btn_PrintAndIncrement";
+            this.btn_PrintAndIncrement.Size = new System.Drawing.Size(120, 23);
+            this.btn_PrintAndIncrement.TabIndex = 2;
+            this.btn_PrintAndIncrement.Text = "Print and Increment";
+            this.btn_PrintAndIncrement.UseVisualStyleBackColor = true;
+            this.btn_PrintAndIncrement.Click += new System.EventHandler(this.btn_PrintAndIncrement_Click);
+            // 
+            // cmbVendor
+            // 
+            this.cmbVendor.FormattingEnabled = true;
+            this.cmbVendor.Items.AddRange(new object[] {
+            "JABBERWOCKY",
+            "THE AMERICAN PIT BBQ"});
+            this.cmbVendor.Location = new System.Drawing.Point(12, 12);
+            this.cmbVendor.Name = "cmbVendor";
+            this.cmbVendor.Size = new System.Drawing.Size(121, 21);
+            this.cmbVendor.TabIndex = 3;
+            this.cmbVendor.Text = "SELECT VENDOR";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cmbVendor);
+            this.Controls.Add(this.btn_PrintAndIncrement);
+            this.Controls.Add(this.nudID);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "frmMain";
+            this.Text = "HTM Food Voucher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudID)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.NumericUpDown nudID;
+        private System.Windows.Forms.Button btn_PrintAndIncrement;
+        private System.Windows.Forms.ComboBox cmbVendor;
+    }
+}
+
---
+++ HTMFoodVoucher/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using RawPrint;
+
+namespace HTMFoodVoucher
+{
+    public partial class frmMain : Form
+    {
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbVendor.SelectedIndex = 0;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            print();
+        }
+
+        private void btn_PrintAndIncrement_Click(object sender, EventArgs e)
+        {
+            print();
+            nudID.Value = nudID.Value + 1;
+        }     
+
+        private void print()
+        {
+            PrinterUtility.EscPosEpsonCommands.EscPosEpson obj = new PrinterUtility.EscPosEpsonCommands.EscPosEpson();
+
+            String sep = "\n------------------------------\n";
+
+            var bytes = logo();
+            
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.FontSelect.FontA());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.Alignment.Center());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes(sep));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleHeight4());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleWidth4());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("\nHackTheMidlands4.0\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("Meal Voucher\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleHeight6());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleWidth6());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes(sep));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("\n" + cmbVendor.Text + "\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes(sep));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleHeight4());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.DoubleWidth4());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("\n" + nudID.Value + "\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes(sep));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, obj.CharSize.Nomarl());
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("\nThis voucher is only redeemable\nat the above vendor.\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("For more information, please\ncontact one of the\norganisers.\n"));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes(sep));
+            bytes = PrinterUtility.PrintExtensions.AddBytes(bytes, Encoding.ASCII.GetBytes("\n\n\n\n"));
+
+            if (File.Exists(".\\tmpPrint.print"))
+            {
+                File.Delete(".\\tmpPrint.print");
+            }
+            File.WriteAllBytes(".\\tmpPrint.print", bytes);
+          
+            IPrinter printer = new Printer();
+            printer.PrintRawFile("POS-58", ".\\tmpPrint.print");
+            try
+            {
+                File.Delete(".\\tmpPrint.print");
+            }
+            catch
+            {
+
+            }
+
+        }
+        
+        private byte[] cutPage()
+        {
+            Console.Out.WriteLine("CUT PAGE");
+            List<byte> obj = new List<byte>();
+            obj.Add(Convert.ToByte(Convert.ToChar(0x1D)));
+            obj.Add(Convert.ToByte('V'));
+            obj.Add((byte)66);
+            obj.Add((byte)3);
+            return obj.ToArray();
+        }
+
+        private byte[] logo()
+        {
+            //  Console.Out.WriteLine("LOGO");
+            //  ImageConverter ic = new ImageConverter();
+            //  return (byte[])ic.ConvertTo(Properties.Resources.htm, typeof(byte[]));
+            //     Image image = Properties.Resources.htm;
+            //     using (var stream = new MemoryStream())
+            //    {
+            //      image.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+            //       return stream.ToArray();
+            //   }
+            return new byte[0];
+
+        }
+
+
+
+    }
+}
